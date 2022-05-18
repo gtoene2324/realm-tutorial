54a4ca12d2425738537b5c992ecbb0d9c3bf4fa8
Grant Toeneboehn <g.toeneboehn@gmail.com>
2022-05-18 19:36:04 -0400

    backend

---
+++ realm-tutorial-backend/hosting/files/precache-manifest.829f69d0b1ee748b57a7ce3fdf90ca58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a839f45e8db95cf595095386f8b066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88f42a49eafe9b7ae8d4",
+    "url": "/static/css/main.c11ef4eb.chunk.css"
+  },
+  {
+    "revision": "b8f700902b827dd1d99c",
+    "url": "/static/js/2.42a7cce1.chunk.js"
+  },
+  {
+    "revision": "ec19a7404159eb2ec065d16f9bab6f00",
+    "url": "/static/js/2.42a7cce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f42a49eafe9b7ae8d4",
+    "url": "/static/js/main.da1f6110.chunk.js"
+  },
+  {
+    "revision": "8d8e7605b29cbcd435f9",
+    "url": "/static/js/runtime-main.62776961.js"
+  }
+]);
